cf106a455dcf403a344fb6c8eb802baf77372b2e
shadowmoonezios <agushifelde@hotmail.com>
2025-09-30 17:45:00 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Mein Buchclub</h1>
+      <p>Willkommen zu unserem Buchclub!</p>
+    </div>
+  );
+}
+
+export default App;
